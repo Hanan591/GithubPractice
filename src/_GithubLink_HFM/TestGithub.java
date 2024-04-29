2cab82a455ec6abe00d26966e72b5a6abb5b2531
Hanan591 <151069406+Hanan591@users.noreply.github.com>
2024-04-29 01:01:49 -0400

    first commit

---
+++ src/_GithubLink_HFM/TestGithub.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package _GithubLink_HFM;
+
+/**
+ * 
+ */
+public class TestGithub {
+	 public static void main(String[] args) {
+	        String myName = "<Hanan Mohamed>"; 
+	        System.out.println("Hello Dr. Johnson! This is " + myName + ".");
+	    }
+	}
+
+
